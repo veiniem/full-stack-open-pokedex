0994dba73e79578ce05790abb00c55d015164494
veiniem <veikka.niemitukia@student.hamk.fi>
2024-02-04 19:50:29 +0200

    11.5

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,10 +23,10 @@ module.exports = {
       "error",
         2
       ],
-    "linebreak-style": [
-      "error",
-      "unix"
-    ],
+    // "linebreak-style": [
+    //   "error",
+    //   "unix"
+    // ],
     "quotes": [
       "error",
       "single"
